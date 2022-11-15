8725ef732129331d5fe1d78fa05092ee93496561
eazeta <emmanuel.2000z@gmail.com>
2022-11-15 21:13:53 +0000

    added profile photo

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,9 +1,10 @@
 import Socials from './Socials';
-
+import MyProfile from '../resources/images/profile.png';
 const AboutMe = () => {
 	return (
 		<div>
 			<h2>Emmanuel Zeta</h2>
+			<img src={MyProfile} />
 			<h3>
 				(+44)7468905860 | <a href='mailto:emmanuel.2000z@gmail.com'>Email Me!</a>
 			</h3>
